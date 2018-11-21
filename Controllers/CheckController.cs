d7644b16b9aed30edf7ecce282e5ee6a81b309f5
Marcus Vinicius Guedes <marcus.guedes@gmail.com>
2018-11-21 20:10:45 -0200

    New commit

--- Controllers/CheckController.cs
+++ Controllers/CheckController.cs
@@ -29,7 +29,7 @@ namespace GeoApi.Controllers
             string path = "Database/GeoLite2-Country.mmdb";
             using (var reader = new Reader(path))
             {
-                Console.WriteLine(Request.Headers["HTTP_X_FORWARDED_FOR"]);
+                Console.WriteLine(Context.Request.Headers["X-Forwarded-For"][0]);
                 Console.WriteLine(_accessor.HttpContext.Connection.RemoteIpAddress.ToString());
                 Console.WriteLine(HttpContext.Connection.RemoteIpAddress.ToString());
                 var data = reader.Find<GeoData>(_accessor.HttpContext.Connection.RemoteIpAddress);
